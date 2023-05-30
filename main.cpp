740f0612098ddd53a514a09d74f621bb282ce8fa
Ray_1024 <skovorodnikovda@mail.ru>
2023-05-30 19:19:40 +0300

    update hal.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <cinttypes>
+#include "hal.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const uint32_t delay_ms{1000};
+const uint32_t led_pins[]{GPIO_PIN_3, GPIO_PIN_4, GPIO_PIN_5,
+                          GPIO_PIN_6, GPIO_PIN_8, GPIO_PIN_9,
+                          GPIO_PIN_11, GPIO_PIN_12};
+const uint32_t sw_pins[]{GPIO_PIN_4, GPIO_PIN_8, GPIO_PIN_10, GPIO_PIN_12};
+
+const uint32_t n{9};
+
+int umain() {
+    while (true) {
+        for (const uint32_t &i: led_pins) {
+            HAL_GPIO_WritePin(GPIOD, i, GPIO_PIN_SET);
+            HAL_Delay(delay_ms);
+            HAL_GPIO_WritePin(GPIOD, i, GPIO_PIN_RESET);
+            HAL_Delay(delay_ms);
+        }
+    }
+
     return 0;
 }
